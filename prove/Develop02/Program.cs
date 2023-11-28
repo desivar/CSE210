97c961f84e3fb506b0bdee5d910ca5baf546bd53
desivar <vdesire36@gmail.com>
2023-11-27 20:08:32 -0600

    add

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,36 @@
 using System;
 
-class Program
+Public class Journal
 {
-    static void Main(string[] args)
+    //variable
+    public List <Journal Entry>_ Journal=new List<Journal eEntry>();
+    private string fileName="MyJournal.txt";
+    private string_userFileName;
+ {public Journal()
+ }
+ public void Display()
+ {
+
+ }
+    Console.WriteLine("\n**********Journal Entries**********);
+    foreach(JournalEntry journalEntry in_Journal)
     {
-        Console.WriteLine("Hello Develop02 World!");
+    journalEntry.Display();
+    }
+
+ Console.WriteLine("\n************End*************")
+
+}
+public void CreateJournalFile()
+{
+    Console.Write("Name of your file?");
+    string userInput=Console.ReadLine();
+    _userFileName=userInput+".txt";
+    if (!CreateJournalFile.Exists(userFileName))
+}
+{
+    foreach(JournalEntry journalEntry in_journal)
+    {
+        outputFile.WriteLine($"{journalEntry._entryNumber};{journalEntry._dateTime};{journlEntry._journalPrompt};{journal Entry._journalEntry}");
     }
 }
