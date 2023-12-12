a49682bded5b00d09904e78e5d8550a7461e1be2
desivar <vdesire36@gmail.com>
2023-12-12 16:09:29 -0600

    program

---
+++ final/Foundation1/program.cs
@@ -0,0 +1,15 @@
+using System;
+class Program
+
+{
+static void Main(string)[]Args)
+
+video[]_video=new video[2];
+
+for (int i = 0;<_video.Length;i ++)
+{
+    Console.WriteLine();
+    _videos[i].DisplayVideoInf();
+    _videos[i].DisplayVideoComments();
+}
+}
