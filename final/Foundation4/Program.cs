954dfac19cd96539d52eb281b9037fe50d09d3a5
desivar <vdesire36@gmail.com>
2023-12-13 15:52:22 -0600

    Foundation4

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Activity> activities =  new List<Activity>();
+        activities.Add(new RunningActivity("24 Feb 2024",6.7,30));
+        activities.Add(new CyclingActivity("18 Oct 2023",9.6,30));
+        activities.Add(new Swimming("24 Dec 2023",96,30));
+
+        int index = 1;
+        foreach(Activity activity in activities)
+        {
+            Console.WriteLine($"\nACTIVITY N°{index}:\n{activity.GetSummary()}");
+            index++;
+        }
+        Console.WriteLine();    
     }
 }
