eec2b791fbcf4aab60ab71f4e0018a51ab59f88d
Desire Delmy Vargas Tinoco <135285744+desivar@users.noreply.github.com>
2024-03-24 21:58:57 -0600

    Update Program.cs

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,35 +1,34 @@
 using System;
-
+// a C# program that simulates an extraction of fake comments from a fake video in YouTube
+  //strings
 class Program
 {
-    static void Main(string[] args)
+     static void Main(string[] args)
     {
         Video[] _videos = new Video[4];
 
-        _videos[0] = new Video("Two Steps From Hell - Victory (Piano Version)",5297 );
-        _video[0].AddComment("");
+        _videos[0] = new Video("EasyTutorials", "How to Play Two Steps from Hell", 330);
+        _videos[0].AddComment("Charles", "thanks a lot!");
+        _videos[0].AddComment("Aramys", "Well done!");
+        _videos[0].AddComment("Mity", "finally a version for begginners");
 
-        _videos[1] = new Video("
-
-
-
-
-How to Build a Hangman Game with Python", 28);
-        _videos[1].AddComment("");
-        _videos[1].AddComment("");
-        _videos[1].AddComment("");
+        _videos[1] = new Video("How to Draw Birds", "Easy tutorial", 980);
+        _videos[1].AddComment("anyf","Easy technique");
+        _videos[1].AddComment("Mark5","This was a great first class!");
+        _videos[1].AddComment("Eny","thank you so much for sharing.That's helpful!!");
         
-        _videos[2] = new Video("The ultimate WATERCOLOUR TUTORIAL | For beginners | Drawlikeasir", 448);
-        _videos[2].AddComment("");
-        _videos[2].AddComment("");
-        _videos[2].AddComment("");
+        _videos[2] = new Video("Wolkenfrei", "Du bist meine Insel", 750);
+        _videos[2].AddComment("Myday", "Impossible to stop listening");
+        _videos[2].AddComment("Luna", "You got a beautifull voice and the song is the perfect definition of love!");
+        _videos[2].AddComment("Gerny9", "Great music, beautiful voice!!! Everything is very harmonious! I enjoyed the song!!! ");
 
-        _videos[3] = new Video("How to draw Anime "Basic Anatomy'' (Anime Drawing Tutorial for Beginners)", 10421);
-        _videos[3].AddComment("");
-        _videos[3].AddComment("");
-        _videos[3].AddComment("");
+        _videos[3] = new Video("Responsive Web Site", "HTML,CSS,JavaScript", 3200);
+        _videos[3].AddComment("cameron", "Great work. Can I know how much time did you spend to do it?");
+        _videos[3].AddComment("luis16", "I like the color palette");
+        _videos[3].AddComment("lu34", "i like this result");
 
-        for(int i = 0; i <
+        for(int i = 0; i < _videos.Length ; i++)
+        {
             Console.WriteLine();
             _videos[i].DisplayVideoInfo();
             _videos[i].DisplayVideoComments();
