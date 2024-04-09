f16e21d44ade3cc6b6ac42af3642dd1f9b7bda5a
Desire Delmy Vargas Tinoco <135285744+desivar@users.noreply.github.com>
2024-04-08 19:30:31 -0600

    end

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,21 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Activity> activities =  new List<Activity>();
+        activities.Add(new RunningActivity("25 April 2021",7.7,30));
+        activities.Add(new CyclingActivity("2 September 2022",9.7,30));
+        activities.Add(new Swimming("17 September 2024",88,30));
+
+        int index = 1;
+        foreach(Activity activity in activities)
+        {
+            Console.WriteLine($"\nACTIVITY N°{index}:\n{activity.GetSummary()}");
+            index++;
+        }
+        Console.WriteLine();    
     }
 }
